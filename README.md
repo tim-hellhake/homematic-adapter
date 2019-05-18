f1f3657430ea3e33137ff55da1ff9b5feb452e12
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2019-05-18 23:54:38 +0200

    Release initial version 0.1.0

---
+++ README.md
@@ -0,0 +1,20 @@
+# HomeMatic Adapter
+
+[![Build Status](https://travis-ci.org/tim-hellhake/homematic-adapter.svg?branch=master)](https://travis-ci.org/tim-hellhake/homematic-adapter)
+[![dependencies](https://david-dm.org/tim-hellhake/homematic-adapter.svg)](https://david-dm.org/tim-hellhake/homematic-adapter)
+[![devDependencies](https://david-dm.org/tim-hellhake/homematic-adapter/dev-status.svg)](https://david-dm.org/tim-hellhake/homematic-adapter?type=dev)
+[![optionalDependencies](https://david-dm.org/tim-hellhake/homematic-adapter/optional-status.svg)](https://david-dm.org/tim-hellhake/homematic-adapter?type=optional)
+[![license](https://img.shields.io/badge/license-MPL--2.0-blue.svg)](LICENSE)
+
+Connect your HomeMatic devices.
+
+## Configuration
+Set the ip address or host name of your ccu in the settings and the port if it deviates from the standard port.
+
+## Supported devices
+- HM-CC-RT-DN
+
+## Teach in device
+- Go to the _Add things_
+- A new thing should appear
+- Add it
---
+++ homematic-adapter.js
@@ -0,0 +1,108 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.*
+ */
+
+'use strict';
+
+const xmlrpc = require('xmlrpc');
+
+const {
+  Adapter,
+  Device,
+  Property
+} = require('gateway-addon');
+
+class RadiatorThermostat extends Device {
+  constructor(adapter, client, address) {
+    super(adapter, `${RadiatorThermostat.name}-${address}`);
+    this['@context'] = 'https://iot.mozilla.org/schemas/';
+    this['@type'] = ['TemperatureSensor'];
+    this.name = 'Radiator thermostat';
+    this.description = 'HomeMatic radiator thermostat';
+    this.client = client;
+    this.address = address;
+
+    this.addProperty({
+      type: 'number',
+      '@type': 'TemperatureProperty',
+      unit: 'degree celsius',
+      title: 'temperature',
+      description: 'The ambient temperature',
+      readOnly: true
+    });
+  }
+
+  addProperty(description) {
+    const property = new Property(this, description.title, description);
+    this.properties.set(description.title, property);
+  }
+
+  startPolling(interval) {
+    this.poll();
+    this.timer = setInterval(() => {
+      this.poll();
+    }, interval * 1000);
+  }
+
+  poll() {
+    // eslint-disable-next-line max-len
+    this.client.methodCall('getValue', [this.address, 'ACTUAL_TEMPERATURE'], (error, value) => {
+      if (!error) {
+        this.updateValue('temperature', value);
+      } else {
+        console.error(`Could not read temperature for ${this.address}`);
+      }
+    });
+  }
+
+  updateValue(name, value) {
+    const property = this.properties.get(name);
+    property.setCachedValue(value);
+    this.notifyPropertyChanged(property);
+  }
+}
+
+class HomeMaticAdapter extends Adapter {
+  constructor(addonManager, manifest) {
+    super(addonManager, HomeMaticAdapter.name, manifest.name);
+    addonManager.addAdapter(this);
+    const host = manifest.moziot.config.host;
+    const port = manifest.moziot.config.port;
+
+    if (!host) {
+      console.warn('Please specify host in the config');
+      return;
+    }
+
+    if (!port) {
+      console.warn('Please specify port in the config');
+      return;
+    }
+
+    const client = xmlrpc.createClient({
+      host,
+      port
+    });
+
+    client.methodCall('listDevices', [], (error, devices) => {
+      if (!error) {
+        for (const device of devices) {
+          // eslint-disable-next-line max-len
+          if (device.PARENT_TYPE === 'HM-CC-RT-DN' && device.TYPE === 'CLIMATECONTROL_RT_TRANSCEIVER') {
+            console.log(`Detected new radiator thermostat ${device.ADDRESS}`);
+            // eslint-disable-next-line max-len
+            const radiatorThermostat = new RadiatorThermostat(this, client, device.ADDRESS);
+            this.handleDeviceAdded(radiatorThermostat);
+            radiatorThermostat.startPolling(1);
+          }
+        }
+      } else {
+        console.error(`Could not enumerate devices: ${error}`);
+      }
+    });
+  }
+}
+
+module.exports = HomeMaticAdapter;
