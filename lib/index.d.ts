d1adf3eb4f1be463625a2682749f99f7ba6fab47
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2020-02-19 21:13:52 +0100

    Remove lib folder

--- lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.*
- */
-import { HomeMaticAdapter } from './homematic-adapter';
-declare const _default: (addonManager: any, manifest: any) => HomeMaticAdapter;
-export = _default;
-//# sourceMappingURL=index.d.ts.map
